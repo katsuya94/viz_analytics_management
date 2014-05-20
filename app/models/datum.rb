0563e39ebd1adb4a5f580b2fcb04b737794c89ac
Adrien Katsuya Tateno <adrien.k.tateno@gmail.com>
2014-05-20 00:37:12 -0500

    Added destroy behavior to datum

--- app/models/datum.rb
+++ app/models/datum.rb
@@ -5,11 +5,18 @@ class Datum < ActiveRecord::Base
 	belongs_to :company
 	belongs_to :metric
 	belongs_to :previous, :class_name => 'Datum'
-	has_one :recent, :dependent => :destroy
+	has_one :recent
 
 	def old?
 		lifetime = self.metric.class.lifetime
 		Date.today - self.created_at.to_date > lifetime
+	end
+
+	before_destroy do |d|
+		if d.recent
+			d.recent.datum = d.previous
+			d.recent.save
+		end
 	end
 
 end
