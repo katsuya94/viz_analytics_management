af36581bf0ea9ad0561f433c6c2edf852c48ec21
Adrien Katsuya Tateno <adrien.k.tateno@gmail.com>
2014-06-02 15:59:47 -0500

    redid equation

--- app/models/definitions/bounce_rate_expected.rb
+++ app/models/definitions/bounce_rate_expected.rb
@@ -5,7 +5,7 @@ class BounceRateExpected < Calculated
 	depends_on :sessions_percent_social
 
 	def self.calculate(sources)
-		0.2278 * sources[:sessions_percent_social] / 100.0 + 0.6562
+		100.0 * (0.2278 * sources[:sessions_percent_social] / 100.0 + 0.6562)
 	end
 
 end
