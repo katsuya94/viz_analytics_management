8aebbfd8017f9877ad9cf841efa9f1eba42a7ed5
Adrien Katsuya Tateno <adrien.k.tateno@gmail.com>
2014-06-02 19:13:13 -0500

    remove expanded platform page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ VizAnalytics::Application.routes.draw do
   get "metrics/dashboard"
   get "metrics/engagement"
   get "metrics/acquisition"
-  get "metrics/platform"
   get "metrics/metric/:metric", to: "metrics#metric"
 end
