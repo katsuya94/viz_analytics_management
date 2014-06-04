000efdb74933a0af4ee9eeaf37e456e3c6981e06
Adrien Katsuya Tateno <adrien.k.tateno@gmail.com>
2014-06-03 23:07:32 -0500

    Modals

--- app/assets/javascripts/metrics.js
+++ app/assets/javascripts/metrics.js
@@ -13,7 +13,6 @@ function suffix(n) {
 
 $(function() {
 	$('.tooltipme').tooltip();
-	$('.popoverme').popover();
 	$('.ajax').each(function(i, el) {
 		$.getJSON(sprintf('/metrics/metric/%s', $(el).attr('metric')), function(data) {
 			var value = $(el).hasClass('smallnum') ? data.value.toFixed(2) : Math.floor(data.value);
